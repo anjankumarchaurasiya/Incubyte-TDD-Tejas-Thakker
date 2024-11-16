717411e67d09fefa6b65fa13f55cd202fbc66349
anjankumarchaurasiya <anjansisteccs2015@gmail.com>
2024-11-16 23:03:51 +0530

    Test: Verify support for multiple numbers

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -8,8 +8,13 @@ describe('String Calculator', () => {
     test('should return the number itself for a single number', () => {
         expect(add("1")).toBe(1);
     });
-    
+
     test('should return the sum of two numbers', () => {
         expect(add("1,2")).toBe(3);
     });
+
+    test('should return the sum of multiple numbers', () => {
+        expect(add("1,2,3,4")).toBe(10);
+    });
+    
 });
