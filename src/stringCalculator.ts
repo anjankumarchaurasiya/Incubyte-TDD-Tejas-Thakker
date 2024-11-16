db87aefb374a90ecd6190c99bbc72b2c84717b03
anjankumarchaurasiya <anjansisteccs2015@gmail.com>
2024-11-16 23:04:44 +0530

    Add: Support newlines as valid delimiters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,4 +1,5 @@
 export function add(numbers: string): number {
     if (!numbers) return 0;
-    return numbers.split(',').map(Number).reduce((sum, num) => sum + num, 0);
+    const delimiter = /,|\n/;
+    return numbers.split(delimiter).map(Number).reduce((sum, num) => sum + num, 0);
 }
--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -17,4 +17,7 @@ describe('String Calculator', () => {
         expect(add("1,2,3,4")).toBe(10);
     });
     
+    test('should handle newlines as delimiters', () => {
+        expect(add("1\n2,3")).toBe(6);
+    });
 });
